41b16c07b6ac7a06e4606b3afc1acd8b9fb22820
a-basuony <basouny85@gmail.com>
2022-12-19 23:13:55 +0200

    update

--- src/App.js
+++ src/App.js
@@ -26,30 +26,29 @@ function App() {
 
   return (
     <div className="app">
-      <h2>let's get started</h2>
-      {/* {expenses.map((ex) => (
-        <ExpenseItem title={ex.title} date={ex.date} amount={ex.amount} />
-      ))} */}
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
+      <h2 className="app-title">let's get started</h2>
+      <div className="expense">
+        <ExpenseItem
+          title={expenses[0].title}
+          date={expenses[0].date}
+          amount={expenses[0].amount}
+        />
+        <ExpenseItem
+          title={expenses[1].title}
+          date={expenses[1].date}
+          amount={expenses[1].amount}
+        />
+        <ExpenseItem
+          title={expenses[2].title}
+          date={expenses[2].date}
+          amount={expenses[2].amount}
+        />
+        <ExpenseItem
+          title={expenses[3].title}
+          date={expenses[3].date}
+          amount={expenses[3].amount}
+        />
+      </div>
     </div>
   );
 }
